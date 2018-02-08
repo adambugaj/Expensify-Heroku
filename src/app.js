5a2e4065cb0220572daa7e475e1fe2e19d11b825
Adam Bugaj <adampeter.bugaj@gmail.com>
2018-02-08 20:28:21 +0100

    Connecting database with firebase

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import { removeExpense } from './actions/expenses';
+import './firebase/firebase';
+
 
 const store = configureStore()
 /*
